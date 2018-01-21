40abbb5074c744263c5569d8e3311b847b11d8e6
jixu90hou <younigengjingcai1992@gmail.com>
2018-01-21 23:34:25 +0800

    add command test

--- rpc-test/src/test/java/hystrix/CommandTest.java
+++ rpc-test/src/test/java/hystrix/CommandTest.java
@@ -1,50 +1,76 @@
 package hystrix;
 
 import java.util.Random;
-import java.util.UUID;
 import java.util.concurrent.*;
+import java.util.function.Function;
 
 interface ICommand {
     boolean execute() throws InterruptedException;
 }
-interface DevCommand{
-    abstract boolean dev() throws TimeoutException;
+
+interface DevCommand {
+    boolean dev() throws TimeoutException;
+}
+
+interface TestCommand {
+    boolean test() throws TimeoutException;
 }
 
 abstract class AbstractCommand implements ICommand {
     private Object lock = new Object();
-    protected volatile boolean testFinished = false;
 
     abstract boolean dev() throws TimeoutException;
 
-    abstract boolean test(boolean testFinished);
+    abstract boolean test() throws TimeoutException;
 
     abstract boolean fixBug();
-    private void retryDev(int times,DevCommand devCommand){
+
+    private boolean retryDev(int times, DevCommand devCommand) {
         Object retryLock = new Object();
+        boolean success = false;
         for (int i = 0; i < times; i++) {
-            boolean success = false;
             try {
                 success = devCommand.dev();
             } catch (TimeoutException e) {
                 //重试一下
-                System.out.println("========重试一下========"+(times-i));
+                System.out.println("========重试一下 dev========" + (times - i));
                 continue;
             }
-            if(success){
-               break; //开发完成，直接退出
+            if (success) {
+                break; //开发完成，直接退出
             }
-            System.out.println("success:"+success);
         }
+        System.out.println("dev success:" + success);
+        return success;
     }
- /*   private void retry(int times, Function<Object, Boolean> function) {
+
+    private boolean retryTest(int times, TestCommand testCommand) {
+        Object retryLock = new Object();
+        boolean success = false;
+        for (int i = 0; i < times; i++) {
+            try {
+                success = testCommand.test();
+            } catch (TimeoutException e) {
+                //重试一下
+                System.out.println("========重试一下 test========" + (times - i));
+                continue;
+            }
+            if (success) {
+                break; //开发完成，直接退出
+            }
+        }
+        System.out.println("test success:" + success);
+        return success;
+    }
+
+    private void retry(int times, Function<Object, Boolean> function) {
         Object retryLock = new Object();
         for (int i = 0; i < times; i++) {
             boolean result = function.apply(null);
             if (!result) {//如果没有开发完成，重试几次
                 synchronized (retryLock) {
                     try {
-                        System.out.println("count down times:" + (times - i));
+                        System.out.println("========重试一下 test========" + (times - i));
                         retryLock.wait(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -55,10 +81,15 @@ abstract class AbstractCommand implements ICommand {
             }
         }
     }
-*/
+
     @Override
     public boolean execute() throws InterruptedException {
-        retryDev(3,()->dev());
+        if (retryDev(5, () -> dev())) {
+            if (retryTest(5, () -> test())){
+                System.out.println("-----test success-----");
+            }
+        }
+
         return true;
     }
 }
@@ -66,44 +97,60 @@ abstract class AbstractCommand implements ICommand {
 class DevCommandA extends AbstractCommand {
     ExecutorService executorService = Executors.newFixedThreadPool(1);
     static TimeoutMessageConsumerQueue timeoutConsumerQueue = new TimeoutMessageConsumerQueue();
+
     static {
         new Thread(timeoutConsumerQueue).start();
     }
 
     @Override
-    boolean dev() throws TimeoutException{
-        System.out.println(".........remote request........");
+    boolean dev() throws TimeoutException {
+        System.out.println(".........remote dev request........");
         boolean success = false;
-        Future<Boolean> future = executorService.submit(() -> response());
+        Future<Boolean> future = executorService.submit(() -> res());
         try {
-           return future.get();
-            //e.printStackTrace();
+            return future.get(3,TimeUnit.SECONDS);
         } catch (InterruptedException e) {
-          //  e.printStackTrace();
+            e.printStackTrace();
         } catch (ExecutionException e) {
-            if(e.getCause() instanceof TimeoutException){
-                throw new TimeoutException();
-            }
+            e.printStackTrace();
         }
         return success;
     }
 
-    public boolean response() throws TimeoutException, InterruptedException {
+ /*   public boolean response() throws TimeoutException, InterruptedException {
         String requestId = UUID.randomUUID().toString();
         timeoutConsumerQueue.put(new TimeoutMessage(requestId, 3, TimeUnit.SECONDS));
         //模拟远程调用消耗时间
-        Random random=new Random();
-        int num=random.nextInt(2)+4;
-        System.out.println("num:"+num);
+        Random random = new Random();
+        int num = random.nextInt(4) + 1;
+        System.out.println("num:" + num);
         TimeUnit.SECONDS.sleep(num);
         timeoutConsumerQueue.checkTimeout(requestId);
         return true;
-    }
+    }*/
 
     @Override
-    boolean test(boolean testFinished) {
-        System.out.println("test success");
+    boolean test() throws TimeoutException {
+        Future<Boolean> future=executorService.submit(()->res());
+        try {
+            return future.get(3,TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         return false;
+    }
+    private boolean res(){
+        try {
+            Random random = new Random();
+            int num = random.nextInt(4) + 1;
+            System.out.println("num:" + num);
+            TimeUnit.SECONDS.sleep(num);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 
     @Override
@@ -122,42 +169,7 @@ public class CommandTest {
     public static void main(String[] args) throws InterruptedException {
         DevCommandA commandA = new DevCommandA();
         boolean success = commandA.execute();
-        System.out.println("success:" + success);
+        System.out.println("execute success:" + success);
     }
-}
-
-
-class Test {
-    private DelayQueue delayQueue = new DelayQueue();
-
-
-    public String print(Integer num) {
-        return "print message " + num;
-    }
-
-    public static void main(String[] args) {
-        Test test = new Test();
-
-        boolean success = test.response();
-        if (success) {
-
-        }
-
-        if (test.response()) {
-
-        }
-        // test.printB((num)->"print "+num);
-        //  test.printA();
-    }
-
-    public boolean response() {
-        try {
-            Thread.sleep(100000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
 }
 
